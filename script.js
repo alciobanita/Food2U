50d59b8502ee81370e4eeb51633531819cf7df1a
Andrei Ciobanita <al.ciobanita@gmail.com>
2024-02-24 13:47:28 +0100

    Update CSS/Footer/shoppingBasket

--- script.js
+++ script.js
@@ -30,17 +30,24 @@ let menus = [
         "description": "Herforder  Bockbier 0.33 l"
     },
     {
-        "name": "Gerolsteiner mit Kohlensäure",
-        "price": "2.80 €",
+        "name": "S. Pellegrino medium",
+        "price": "4.80 €",
         "amount": "1",
         "description": "Wasser mit Kohlensäure 1 l"
+    },
+    {
+        "name": "S. Pellegrino still",
+        "price": "3.80 €",
+        "amount": "1",
+        "description": "Wasser ohne Kohlensäure 1 l"
     }
 ]
 
 let shoppingBasket = [
     {
         "names": [],
-        "prices": []
+        "prices": [],
+        "quantities": []
     }
 ];
 
@@ -56,7 +63,7 @@ function render() {
     for (let i = 0; i < menus.length; i++) {
         const menu = menus[i];
         menuList.innerHTML += /*html*/`
-            <div class="card" onclick="addToBasket()">
+            <div class="card" onclick="addToBasket('${menu['name']}', ${parseFloat(menu['price'])})">
                 <div class="menuDescription">
                     <h2>${menu['name']}</h2>
                     <p>${menu['description']}</p>
@@ -75,7 +82,6 @@ function generateIndexHTML() {
             <div class="wp-name" onclick="goToHomepage()">
                 <img class="logo" src="./icon/besteck.png" class="AppIcon"><h1>Food2U</h1>
             </div>
-            <img class="logoBasket" src="./icon/basket.png" onclick="showBasket()">
         </div>
         <div class="restaurant">
             <h1 class="restName" id="r-Name">Pizzeria Belladona</h1>
@@ -84,58 +90,80 @@ function generateIndexHTML() {
             <div id="menuList" class="menusArea"></div>
             <div id="openBasket" class="basketArea">
                 <h3>Warenkorb</h3>
-                <div class="spacer"></div>
-                <div class="items" id="basketContent">
-                    <h4>Fülle deinen Warenkorb</h3>
-                    <span class="infoBasket">Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</span>
-                </div>
-                <div class="spacer"></div>
-                <div id="basketValue">Bestellwert: <span id="totalPrice">0.00</span> €</div>
-                <div class="spacer"></div>
-                <button type="submit" class="btn btn-primary" onclick="sendOrder()">Bestellen</button>
+                <div class="basketStyle">
+                    <div class="spacer"></div>
+                    <div class="items" id="basketContent">
+                        <h4>Fülle deinen Warenkorb</h3>
+                        <span class="infoBasket">Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle dein Essen.</span>
+                    </div>
+                    <div class="spacer"></div>
+                    <div id="basketValue">Bestellwert: <span id="totalPrice">0.00</span> €</div>
+                    <div class="spacer"></div>
+                    <p class="message">Zu deiner Bestellung werden 3,95 € Versandkosten berechnet.</p><br><br>
+                    <button type="submit" class="btn btn-primary" onclick="sendOrder()">Bestellen</button>
+            
             </div>
         </div>
-        `;
-        generateBasketHTML();
-}
-
-function generateBasketHTML(){
-    return /*html*/`
-
+        <div class="footer">
+            &copy; Food2U 2024  <div><a href="#impressum">Impressum</a> |  <a href="#datenschutz">Datenschutzerklärung</a></div>
+        </div>
+        </div>
     `;
 }
 
 function goToHomepage() {
-    render();
-    updateShoppingBusket();
+    window.location = "index.html";
 }
 
 function addToBasket(name, price) {
-    for (let j = 0; j < menus['name'].length; j++) {
-        let name = menus['name'][j];
-
-        shoppingBasket['names'].push(name);
+    let index = shoppingBasket[0].names.indexOf(name);
+    if (index === -1) {
+        shoppingBasket[0].names.push(name);
+        shoppingBasket[0].prices.push(price);
+        shoppingBasket[0].quantities.push(1);
+    } else {
+        shoppingBasket[0].prices[index] += price;
+        shoppingBasket[0].quantities[index]++;
     }
-
-    for (let t = 0; t < menus['price'].length; t++) {
-        let price = menus['price'][t];
-
-        shoppingBasket['prices'].push(price);
-    }
-    updateShoppigBasket();
-    console.log('Hallo!');
+    updateShoppingBusket();
 }
-
 function updateShoppingBusket() {
     let sum = 0.00;
-    for (let i = 0; i < shoppingBasket['prices'].length; i++) {
-        sum += shoppingBasket['prices'][i];
-    }
+    let listHtml = '';
+    let items = shoppingBasket[0].names.map((name, index) => {
+        let price = shoppingBasket[0].prices[index];
+        let quantity = shoppingBasket[0].quantities[index];
+        let totalPrice = price;
+        sum += totalPrice;
+        return `<div class="basketItem">${name} x ${quantity} = ${totalPrice.toFixed(2)} €</div>`;
+    });
 
     let finalSum = sum + 3.95;
-    document.getElementById('totalPrice').innerHTML = finalSum;
+    if (shoppingBasket[0].names.length > 0) {
+        document.getElementById('totalPrice').innerHTML = finalSum.toFixed(2);
+    }
+    document.getElementById('basketContent').innerHTML = listHtml.concat(items.join(''));
 }
 
 function showBasket() {
-    addToBasket();
+    basket = document.getElementById('openBasket');
+    let menuList = document.getElementById('menuList');
+    basket.style.display = 'block';
+    menuList.style.display = 'none';
+
+    let sum = 0.00;
+    let listHtml = '';
+    let items = shoppingBasket[0].names.map((name, index) => {
+        let price = shoppingBasket[0].prices[index];
+        let quantity = shoppingBasket[0].quantities[index];
+        let totalPrice = price * quantity;
+        sum += totalPrice;
+        return `<div class="basketItem">${name} x ${quantity} = ${totalPrice.toFixed(2)} €</div>`;
+    });
+
+    let finalSum = sum + 3.95;
+    if (shoppingBasket[0].names.length > 0) {
+        document.getElementById('totalPrice').innerHTML = finalSum.toFixed(2);
+    }
+    document.getElementById('basketContent').innerHTML = listHtml.concat(items.join(''));
 }
